2fafb757973bb9953fc2e0af7d4e685a3c08e67d
mrdkap <mrdkap@gmail.com>
2013-11-15 06:57:42 -0500

    Updated to allow for the various different con possibilities.

--- index.php
+++ index.php
@@ -16,11 +16,12 @@ mysql_select_db($ReportDB,$link);
 $query= <<<EOF
 SELECT
     conname,
-    constartdate
+    constartdate,
+    conid
   FROM
       ConInfo
-  WHERE
-      conid=$ConKey
+  ORDER BY
+    constartdate DESC
 EOF;
 
 // Retrieve query fail if database can't be found, and if there isn't just one result
@@ -29,34 +30,30 @@ if (($result=mysql_query($query,$link))===false) {
   $message.=$query;
   $message.="</P>\n";
 }
-if (0==($rows=mysql_num_rows($result))) {
+if (0==($conrows=mysql_num_rows($result))) {
   $message.="<P>No results found.</P>\n<P>";
   $message.=$query;
   $message.="</P>\n";
 }
-if (1<($rows=mysql_num_rows($result))) {
-  $message.="<P>Too many results found.</P>\n<P>";
-  $message.=$query;
-  $message.="</P>\n";
+
+for ($i=1; $i<=$conrows; $i++) {
+  $tmpconinfo_array=mysql_fetch_assoc($result);
+  $ConCount_array[$i]=$tmpconinfo_array['conid'];
+  $ConInfo_array[$tmpconinfo_array['conid']]['conname']=$tmpconinfo_array['conname'];
+  $ConInfo_array[$tmpconinfo_array['conid']]['constartdate']=$tmpconinfo_array['constartdate'];
 }
-$ConInfo_array=mysql_fetch_assoc($result);
 
-$ConName=$ConInfo_array['conname'];
-$ConStartDate=$ConInfo_array['constartdate'];
-
-$constart=strtotime($ConStartDate);
 $nowis=time();
 
 // Establish the states, for the look of the page
 $query= <<<EOF
 SELECT
     phasestate,
-    phasetypename
+    phasetypename,
+    conid
   FROM
       Phase
     JOIN PhaseTypes USING (phasetypeid)
-  WHERE
-    conid=$ConKey
 EOF;
 
 // Retrieve query, fail if database can't be found, or there aren't any results
@@ -65,88 +62,88 @@ if (($result=mysql_query($query,$link))===false) {
   $message.=$query;
   $message.="</P>\n";
 }
-if (0==($rows=mysql_num_rows($result))) {
+if (0==($phaserows=mysql_num_rows($result))) {
   $message.="<P>No results found.</P>\n<P>";
   $message.=$query;
   $message.="</P>\n";
 }
 
 // Set up the phase_array such that the typename is the key and the state is the value
-for ($i=1; $i<=$rows; $i++) {
+for ($i=1; $i<=$phaserows; $i++) {
   $element_array=mysql_fetch_assoc($result);
-  $phase_array[$element_array['phasetypename']]=$element_array['phasestate'];
+  $phase_array[$element_array['conid']][$element_array['phasetypename']]=$element_array['phasestate'];
 }
 
-// These should be passed in from the calling page, if they aren't use the local copies
-if (!isset($ConStaffBios)) {$ConStaffBios="webpages/ConStaffBios.php";}
-if (!isset($Postgrid)) {$Postgrid="webpages/Postgrid.php";}
-if (!isset($Descriptions)) {$Descriptions="webpages/Descriptions.php";}
-if (!isset($Schedule)) {$Schedule="webpages/Schedule.php";}
-if (!isset($Tracks)) {$Tracks="webpages/Tracks.php";}
-if (!isset($Bios)) {$Bios="webpages/Bios.php";}
-if (!isset($Vendors)) {$Vendors="webpages/Vendors.php";}
-if (!isset($Feedback)) {$Feedback="webpages/Feedback.php";}
+$title="Zambia -- ". $ConInfo_array[$ConKey]['conname'];
 
+$onetime=0;
+for ($i=1; $i<=$conrows; $i++) {
+  $conid=$ConCount_array[$i];
+  $constart=strtotime($ConInfo_array[$conid]['constartdate']);
+  $conname=htmlspecialchars_decode($ConInfo_array[$conid]['conname']);
 
-$title="Zambia -- ". $ConInfo_array['conname'];
-
-if ($nowis < $constart) { 
-  $webstring.="<H2>Check out the below links to learn about the great programming and vending we will have at $ConName!</H2>\n";
-} else { 
-  $webstring.="<H2>Check out the below links to give us/see your feedback and learn about the great programming we had at $ConName!</H2>\n";
-}
-$webstring.="<UL>\n";
-$webstring.="  <LI><A HREF=\"$ConStaffBios\">Con Staff</A></LI>\n";
-if ($phase_array['Prog Available'] == '0' ) {
-  $webstring.="  <LI><A HREF=\"$Postgrid\">Schedule Grid</A></LI>\n";
-  $webstring.="  <LI><A HREF=\"$Descriptions\">Class Descriptions</A></LI>\n";
-  $webstring.="  <LI><A HREF=\"$Schedule\">Schedule</A></LI>\n";
-  $webstring.="  <LI><A HREF=\"$Tracks\">Tracks</A></LI>\n";
-  $webstring.="  <LI><A HREF=\"$Bios\">Presenter Bios</A></LI>\n";
-}
-if ($phase_array['Vendors Available'] == '0' ) {
-  $webstring.="  <LI><A HREF=\"$Vendors\">Vendor List</A></LI>\n";
-}
-if ($phase_array['Vol Available'] == '0' ) {
-  $webstring.="  <LI><A HREF=\"$Postgrid?volunteer=y\">Volunteer Grid</A></LI>\n";
-  $webstring.="  <LI><A HREF=\"$Descriptions?volunteer=y\">Volunteer Job Descriptions</A></LI>\n";
-}
-if ($nowis < $constart) { 
-  $webstring.="  <LI><A HREF=\"webpages/\">Presenter/Vendor Login</A></LI>\n";
-  if ($phase_array['Brainstorm'] == '0' ) {
-    $webstring.="  <LI>\n";
-    $webstring.="  <FORM name=\"brainstormform\" method=\"POST\" action=\"webpages/doLogin.php\">\n";
-    $webstring.="    <INPUT type=\"hidden\" name=\"badgeid\" value=\"100\">\n";
-    $webstring.="    <INPUT type=\"hidden\" name=\"passwd\" value=\"submit\">\n";
-    $webstring.="    <INPUT type=\"hidden\" name=\"target\" value=\"brainstorm\">\n";
-    $webstring.="    <INPUT type=\"submit\" name=\"submit\" value=\"Class/Presenter Submission\">\n";
-    $webstring.="  </FORM>\n";
+  if ($nowis < $constart) { 
+    $webstring.="<H3>Check out the below links to learn about the great programming and vending we will have at $conname!</H3>\n";
+  } else {
+    if ($onetime < 1) {
+      $webstring.="<HR>\n<H3>Check out the below links to learn about the great programming we had at previous events:</H3>\n<HR>\n";
+      $onetime++;
+    }
+    $webstring.="<H3>$conname:</H3>\n";
   }
-  if ($phase_array['Vendor'] == '0' ) {
-    $webstring.="  <LI>\n";
-    $webstring.="  <FORM name=\"vendorform\" method=\"POST\" action=\"webpages/doLogin.php\">\n";
-    $webstring.="    <INPUT type=\"hidden\" name=\"badgeid\" value=\"100\">\n";
-    $webstring.="    <INPUT type=\"hidden\" name=\"passwd\" value=\"submit\">\n";
-    $webstring.="    <INPUT type=\"hidden\" name=\"target\" value=\"vendor\">\n";
-    $webstring.="    <INPUT type=\"submit\" name=\"submit\" value=\"New Vendor Application\">\n";
-    $webstring.="  </FORM>\n";
+  $webstring.="<UL>\n";
+  $webstring.="  <LI><A HREF=\"webpages/ConStaffBios.php?conid=$conid\">Con Staff</A></LI>\n";
+  if ($phase_array[$conid]['Prog Available'] == '0' ) {
+    $webstring.="  <LI><A HREF=\"webpages/Postgrid.php?conid=$conid\">Schedule Grid</A></LI>\n";
+    $webstring.="  <LI><A HREF=\"webpages/Descriptions.php?conid=$conid\">Class Descriptions</A></LI>\n";
+    $webstring.="  <LI><A HREF=\"webpages/Schedule.php?conid=$conid\">Schedule</A></LI>\n";
+    $webstring.="  <LI><A HREF=\"webpages/Tracks.php?conid=$conid\">Tracks</A></LI>\n";
+    $webstring.="  <LI><A HREF=\"webpages/Bios.php?conid=$conid\">Presenter Bios</A></LI>\n";
   }
-} else { 
-  if ($phase_array['Feedback Available'] == '0') {
-    $webstring.="  <LI><A HREF=\"$Feedback\">Feedback</A></LI>\n";
+  if ($phase_array[$conid]['Vendors Available'] == '0' ) {
+    $webstring.="  <LI><A HREF=\"webpages/Vendors.php?conid=$conid\">Vendor List</A></LI>\n";
   }
-
-	// Percy, we need to make a hard link to Zambia or run this through Pyro,  right now, the link breaks
-  $webstring.="  <LI><A HREF=\"webpages/\">Presenter Login</A></LI>\n";
+  if ($phase_array[$conid]['Vol Available'] == '0' ) {
+    $webstring.="  <LI><A HREF=\"webpages/Postgrid.php?volunteer=y&conid=$conid\">Volunteer Grid</A></LI>\n";
+    $webstring.="  <LI><A HREF=\"webpages/Descriptions.php?volunteer=y&conid=$conid\">Volunteer Job Descriptions</A></LI>\n";
+  }
+  if ($nowis < $constart) { 
+    $webstring.="  <LI><A HREF=\"webpages/\">Presenter/Vendor Login</A></LI>\n";
+    if ($phase_array[$conid]['Brainstorm'] == '0' ) {
+      $webstring.="  <LI>\n";
+      $webstring.="  <FORM name=\"brainstormform\" method=\"POST\" action=\"webpages/doLogin.php\">\n";
+      $webstring.="    <INPUT type=\"hidden\" name=\"badgeid\" value=\"100\">\n";
+      $webstring.="    <INPUT type=\"hidden\" name=\"passwd\" value=\"submit\">\n";
+      $webstring.="    <INPUT type=\"hidden\" name=\"target\" value=\"brainstorm\">\n";
+      $webstring.="    <INPUT type=\"submit\" name=\"submit\" value=\"Class/Presenter Submission\">\n";
+      $webstring.="  </FORM>\n";
+    }
+    if ($phase_array[$conid]['Vendor'] == '0' ) {
+      $webstring.="  <LI>\n";
+      $webstring.="  <FORM name=\"vendorform\" method=\"POST\" action=\"webpages/doLogin.php\">\n";
+      $webstring.="    <INPUT type=\"hidden\" name=\"badgeid\" value=\"100\">\n";
+      $webstring.="    <INPUT type=\"hidden\" name=\"passwd\" value=\"submit\">\n";
+      $webstring.="    <INPUT type=\"hidden\" name=\"target\" value=\"vendor\">\n";
+      $webstring.="    <INPUT type=\"submit\" name=\"submit\" value=\"New Vendor Application\">\n";
+      $webstring.="  </FORM>\n";
+    }
+  } else { 
+    if ($phase_array[$conid]['Feedback Available'] == '0') {
+      $webstring.="  <LI><A HREF=\"webpages/Feedback.php?conid=$conid\">Feedback</A></LI>\n";
+    }
+    // Percy, we need to make a hard link to Zambia or run this through Pyro,  right now, the link breaks
+    if ($onetime < 1) {
+    $webstring.="  <LI><A HREF=\"webpages/\">Presenter Login</A></LI>\n";
+    }
+  }
+  $webstring.="</UL>\n";
 }
-$webstring.="</UL>\n";
+
 
 // Percy, Added $include IF eval
 // -Mike
-
-	if (!isset($included) && $included != 'YES')
-	{
-	// end new IF $included
+if (!isset($included) && $included != 'YES')
+  {
 	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -173,6 +170,7 @@ include ('webpages/google_analytics.php');
 ?>
 </body>
 </html>
+
 <?php
 }  // close IF $included 
 ?>

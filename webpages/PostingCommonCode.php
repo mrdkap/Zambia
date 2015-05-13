a1baebecd676d50ce9e44e1f61f866b55b6da378
mrdkap <mrdkap@gmail.com>
2015-05-13 00:10:36 -0400

    Fix for the public pages from eliminated CON_KEY and ConKey from CommonCode.

--- webpages/PostingCommonCode.php
+++ webpages/PostingCommonCode.php
@@ -2,7 +2,25 @@
     $logging_in=true;
     $_SESSION['permission_set'][0] = "none";
     $_SESSION['role']="Posting";
+    if ((empty($conid)) and (is_numeric($_GET['conid']))) {
+      $conid=$_GET['conid'];
+    }
+    if ((empty($conid)) and (is_numeric($_SESSION['conid']))) {
+      $conid=$_SESSION['conid'];
+    }
+    if ((empty($_SESSION['conid'])) and (is_numeric($conid))) {
+      $_SESSION['conid']=$conid;
+    }
     require_once('CommonCode.php');
     $_SESSION['permission_set'][0] = "none";
     $_SESSION['role']="Posting";
+    if ((empty($conid)) and (is_numeric($_GET['conid']))) {
+      $conid=$_GET['conid'];
+    }
+    if ((empty($conid)) and (is_numeric($_SESSION['conid']))) {
+      $conid=$_SESSION['conid'];
+    }
+    if ((empty($_SESSION['conid'])) and (is_numeric($conid))) {
+      $_SESSION['conid']=$conid;
+    }
 ?>

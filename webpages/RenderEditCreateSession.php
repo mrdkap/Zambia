b287ae53f56890dfdd7c8058568d33ec6dc43659
Percival <percyhaven@gmail.com>
2017-07-21 22:06:04 -0400

    Added the email address and the mapping to name that seems to have gotten lost somewhere along the way, back into RenderEditCreateSession.

--- webpages/RenderEditCreateSession.php
+++ webpages/RenderEditCreateSession.php
@@ -9,9 +9,14 @@ function RenderEditCreateSession ($action, $session, $message, $message_error) {
   global $link, $name, $email, $debug;
   $conid=$_SESSION['conid'];
   $badgeid=$_SESSION['badgeid'];
+  $name=$_SESSION['badgename'];
   require_once("CommonCode.php");
   require_once("javascript_functions.php");
 
+
+  $emailquery="SELECT email from CongoDump where badgeid=$badgeid";
+  list($email_rows,$email_header_array,$email_array)=queryreport($emailquery,$link,$title,$emailquery,0);
+  $email=$email_array[1]["email"];
 
   // Limit the types of submissions we are accpting at this time.
   $typequery = <<<EOD
